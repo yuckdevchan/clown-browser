ab05eaf486ebe72ed775315735d0227fe191a9d8
yuck <60288171+yuckdevchan@users.noreply.github.com>
2022-10-12 15:46:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
+import time
 
 print("Clown Browser v0.1")
 
-url_input = input("What website do you want to go to?: ")
-
-r = requests.get(url_input, allow_redirects=True)
-print("Downloaded '" + url_input + "' from the internet")
+def url():
+  url_input = input("What website do you want to go to?: ")
+  
+  start = time.process_time()
+  r = requests.get(url_input, allow_redirects=True)
+  
+  print("Downloaded '" + url_input + "' from the internet in" + time.process_time() - start)
