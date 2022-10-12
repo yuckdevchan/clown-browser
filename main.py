63f23c53d1b587c533a467a89b7be94f495f6223
yuck <60288171+yuckdevchan@users.noreply.github.com>
2022-10-12 19:51:26 +0100

    fix some stuff and make the program overall more flexible

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-import requests, time, os
+import urllib3.request, time, os
 
-print("Clown Browser v0.1")
+print("Clown Browser v0.1\n")
+
+def dir_gen():
+  clown_folder = os.path.dirname(os.path.realpath(__file__))
+  path = os.path.join(clown_folder, "webcache")
+  if os.path.isdir(path) == False:
+    os.mkdir(path)
+  else:
+    pass
+
+dir_gen()
 
 def url():
+
+  clown_folder = os.path.dirname(os.path.realpath(__file__))
+
   url_input = input("What website do you want to go to?: ")
 
   if url_input.startswith("https://") == True:
-    print("Passed Check #1: Is a valid URL, of type: " + "HTTPS (Hypertext Transfer Protocol Secure)")
+    print("\nPassed Check #1: Is a valid URL, of type: " + "HTTPS (Hypertext Transfer Protocol Secure)")
   elif url_input.startswith("http://") == True:
     print("Passed Check #1: Is a valid URL, of type: " + "HTTP (Hypertext Transfer Protocol (INSECURE))")
   else:
-    print("Failed Check #1: Is not a valid URL, of type HTTPS or HTTPS")
-    print("Exiting")
+    print("\nFailed Check #1: Is not a valid URL, of type HTTPS or HTTPS")
+    print("\nExiting")
     for i in range(3, 0, -1):
       time.sleep(1)
       print(i)
     exit()
 
   start = time.process_time()
-  r = requests.get(url_input, allow_redirects=True)
+  http = urllib3.PoolManager()
+  r = http.request('GET', url_input)
+  print("Website returned code: " + str(r.status))
+  
+  path = os.path.join(clown_folder, "webcache", url_input.split("//")[1])
+  if os.path.isdir(path) == False:
+    os.mkdir(path)
+  else:
+    pass
   
   print("Downloaded '" + url_input + "' from the internet in " + str(time.process_time() - start) + " seconds.")
+  
+  index_input = input("Continue to index.html? (y, n): ")
+
+  if index_input == "y":
+    print("Downloading index.html...")
+    index_load_time = time.process_time()
+    
+    print("Downloaded 'index.html' from the internet in " + str(time.process_time() - start) + " seconds.")
 
 url()
