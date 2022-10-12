ff3ff2a9d231d2d01979c350176cb4d8cf5376cb
yuck <60288171+yuckdevchan@users.noreply.github.com>
2022-10-12 18:11:30 +0100

    some updates

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import requests
-import time
+import requests, time, os
 
 print("Clown Browser v0.1")
 
 def url():
   url_input = input("What website do you want to go to?: ")
-  
+
+  if url_input.startswith("https://") == True:
+    print("Passed Check #1: Is a valid URL, of type: " + "HTTPS (Hypertext Transfer Protocol Secure)")
+  elif url_input.startswith("http://") == True:
+    print("Passed Check #1: Is a valid URL, of type: " + "HTTP (Hypertext Transfer Protocol (INSECURE))")
+  else:
+    print("Failed Check #1: Is not a valid URL, of type HTTPS or HTTPS")
+    print("Exiting")
+    for i in range(3, 0, -1):
+      time.sleep(1)
+      print(i)
+    exit()
+
   start = time.process_time()
   r = requests.get(url_input, allow_redirects=True)
   
-  print("Downloaded '" + url_input + "' from the internet in" + time.process_time() - start)
+  print("Downloaded '" + url_input + "' from the internet in " + str(time.process_time() - start) + " seconds.")
+
+url()
