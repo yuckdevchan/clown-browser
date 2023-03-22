ed784696c7e2119b57ae350ddf71f7add8a577d8
yuck <60288171+yuckdevchan@users.noreply.github.com>
2023-03-22 19:01:08 +0000

    Add joker (extra jester stuff)
    Also move fail() function to new joker.py

--- circus/jester.py
+++ circus/jester.py
@@ -1,11 +1,10 @@
 # Circus Web Engine (ɔ) 2023
 import os
+from circus.joker import html_render_fail
 
-def fail(error_reason):
-    print(f"Failed to render HTML!\nError Code: {error_reason}")
+def validation(html_file):
 
 def render_html(html_file):
-    print("Circus: Rendering HTML...")
+    print("Circus[Jester]: Rendering HTML...")
     if os.stat(html_file).st_size == 0:
         fail("HTML File is Empty!")
-        
---
+++ circus/joker.py
@@ -0,0 +1,2 @@
+def html_render_fail(error_reason):
+    print(f"Circus[Joker]: Failed to render HTML!\nError Code: {error_reason}")
