20eefb36c95882e3d2a07a43c24079c9a040f89e
yuck <60288171+yuckdevchan@users.noreply.github.com>
2023-03-22 18:56:01 +0000

    Remove duplicate -r argparse flag

--- puppets/marionette.py
+++ puppets/marionette.py
@@ -11,7 +11,6 @@ def cli_args(program_name, program_desc):
   arg_group.add_argument('-i', '--input', metavar="", help="Specify and input URL or URI")
   arg_group.add_argument('-c', '--cache', metavar="", help="Specify and cache folder")
   # Inputs that are on/off flags
-  arg_group.add_argument('-r', '--reset', action="store_true", help="Reset Clown Browser")
   arg_group.add_argument('-q', '--quiet', action="store_true", help="Make output less verbose")
   arg_group.add_argument('-v', '--verbose', action="store_true", help="Make output more verbose")
   arg_group.add_argument('-r', '--reset', action="store_true", help="Reset Clown Browser")
