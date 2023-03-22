47c334b1c9123e7395a9b925facc3a4ed2fa0f53
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-03-22 16:21:00 +0000

    add lots of args in ringmaster

---
+++ puppets/ringmaster.py
@@ -0,0 +1,25 @@
+# Ringmaster - file that interprets command line arguments
+import argparse
+from puppets.reset import reset_app
+
+def cli_args(program_name, program_desc):
+  parser = argparse.ArgumentParser(
+    prog=program_name,
+    description=program_desc)
+  arg_group = parser.add_mutually_exclusive_group()
+  # Inputs that specify a value
+  arg_group.add_argument('-i', '--input', help="Specify and input URL or URI")
+  arg_group.add_argument('-c', '--cache', help="Specify and cache folder")
+  # Inputs that are on/off flags
+  arg_group.add_argument('-r', '--reset', action="store_true", help="Reset Clown Browser")
+  arg_group.add_argument('-q', '--quiet', action="store_true", help="Make output less verbose")
+  arg_group.add_argument('-v', '--verbose', action="store_true", help="Make output more verbose")
+  
+  print(parser.parse_args())
+  args = parser.parse_args()
+  
+  if args.reset:
+    reset_app()
+  else:
+    pass
+  
