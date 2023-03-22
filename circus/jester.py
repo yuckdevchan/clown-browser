e1cd68da7664040dfab31cad2bbafba389487f33
yuck <60288171+yuckdevchan@users.noreply.github.com>
2023-03-22 19:03:06 +0000

    Move empty file check to joker.py

--- circus/jester.py
+++ circus/jester.py
@@ -3,8 +3,8 @@ import os
 from circus.joker import html_render_fail
 
 def validation(html_file):
+    if os.stat(html_file).st_size == 0:
+        fail("HTML File is Empty!")
 
 def render_html(html_file):
     print("Circus[Jester]: Rendering HTML...")
-    if os.stat(html_file).st_size == 0:
-        fail("HTML File is Empty!")
--- circus/joker.py
+++ circus/joker.py
@@ -1,2 +1,6 @@
 def html_render_fail(error_reason):
-    print(f"Circus[Joker]: Failed to render HTML!\nError Code: {error_reason}")
+    print(f"Circus[Joker]: Failed to render HTML!\nError: {error_reason}")
+
+def empty_file_check(html_file):
+    if os.stat(html_file).st_size == 0:
+        html_render_fail("HTML File is Empty!")
