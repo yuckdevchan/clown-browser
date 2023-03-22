d729565dbc573eee4a1717ec1ea69c45d0eedb54
yuck <60288171+yuckdevchan@users.noreply.github.com>
2023-03-22 18:52:15 +0000

    Add empty metavars to marionette
    This makes the `-h` output much easier to read.

--- puppets/marionette.py
+++ puppets/marionette.py
@@ -8,8 +8,8 @@ def cli_args(program_name, program_desc):
     description=program_desc)
   arg_group = parser.add_mutually_exclusive_group()
   # Inputs that specify a value
-  arg_group.add_argument('-i', '--input', help="Specify and input URL or URI")
-  arg_group.add_argument('-c', '--cache', help="Specify and cache folder")
+  arg_group.add_argument('-i', '--input', metavar="", help="Specify and input URL or URI")
+  arg_group.add_argument('-c', '--cache', metavar="", help="Specify and cache folder")
   # Inputs that are on/off flags
   arg_group.add_argument('-r', '--reset', action="store_true", help="Reset Clown Browser")
   arg_group.add_argument('-q', '--quiet', action="store_true", help="Make output less verbose")
