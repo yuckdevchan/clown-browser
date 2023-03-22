08ad7f515063ec782c26c6155cf65bee68aa4ceb
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-03-22 16:24:35 +0000

    add CLI args

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import from other files
 from puppets.reset import reset_app
+from puppets.marionette import cli_args
 from comedious.comedy import MainWindow
 from comedious.funny import app_version
 # Import required Qt and other stuff
@@ -7,6 +8,8 @@ import sys, argparse, colorama
 from colorama import Fore
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication
+
+cli_args()
 
 print(Fore.MAGENTA + f"Clown Browser {app_version}\n" + Fore.WHITE)
 
