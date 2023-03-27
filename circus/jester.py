32af0436bdd2a93a158f112c1f6642911e005f7f
yuck <60288171+yuckdevchan@users.noreply.github.com>
2023-03-27 20:10:55 +0100

    Add quotes around HTML title in terminal

--- circus/jester.py
+++ circus/jester.py
@@ -16,5 +16,5 @@ def render_html(html_file):
         file_contents = "".join(f.readlines()) 
     soup_url = BeautifulSoup(file_contents, "html.parser")
     website_element_title = soup_url.find("title").get_text()
-    print(f"Circus[Jester]: " + Fore.BLUE + "HTML title: " + Fore.MAGENTA + website_element_title + Fore.WHITE)
+    print(f"Circus[Jester]: " + Fore.BLUE + "HTML title: " + Fore.WHITE + "'" + Fore.MAGENTA + website_element_title + Fore.WHITE + "'")
     
